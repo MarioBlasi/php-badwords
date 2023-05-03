428c50551284ae337d2746cc91ed58d89840d621
MarioBlasi <mblasi509@gmail.com>
2023-05-03 16:59:17 +0100

    stamp paragrafo e lunghezza parola censurata

--- myscript.php
+++ myscript.php
@@ -1,13 +1,44 @@
 
-    <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <!--Script Bootstrap bundle-->
+</head>
+<body >
 
-    $paragrafo = $_POST["paragrafo"];
-    $censura = $_POST["censura"];
+  <div class="container mt-5">
+    <div class="row d-flex justify-content-start ">
+        <div class="col col-6 shadow p-5">
+            <?php
+
+            var_dump($_GET);
+
+                $paragrafo = $_GET["paragrafo"];
+                $censura = $_GET["censura"];
 
 
-    // Stampiamo il paragrafo e la sua lunghezza
-    echo "<p>Il tuo paragrafo è:</p>";
-    echo "<p>$paragrafo</p>";
+                // Stampiamo il paragrafo e la sua lunghezza
+            
+            
+                echo "<h1>Il tuo paragrafo è:</h1>";
+                echo $paragrafo;
 
-    ?>
-
+                echo "<h1>parola censurata:</h1>";
+                echo "<p>***</p>";
+            ?>
+        </div>
+    </div>
+  </div>
+    
+</body>
+</html>
